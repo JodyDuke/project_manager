ab50383fdbbcf0d99b675b3c8332327e7174f118
Jody Duke <contact@jodyduke.co.uk>
2018-06-08 15:30:26 +0100

    added local state and submit action to form

--- src/components/modals/new_project/new_project.js
+++ src/components/modals/new_project/new_project.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { currentModal } from '../../../redux/actions';
+import { currentModal, addProject } from '../../../redux/actions';
+import Button from '../../buttons'
 
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        currentModal: modal => dispatch(currentModal(modal))
+        currentModal: modal => dispatch(currentModal(modal)),
+        addProject: project => dispatch(addProject(project))
     }
 }
 
 class ConnectedNewProject extends Component {
     constructor(){
         super()
+
+        this.state = {
+            projectName: '',
+            projectDetails: {}
+        }
+
         this.handleClose = this.handleClose.bind(this)
         this.handleBackgroundClose = this.handleBackgroundClose.bind(this)
+        this.handleInputChange = this.handleInputChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleInputChange(e) {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
+
+    handleSubmit(e) {
+        this.props.addProject(this.state)
     }
 
     handleClose(e) {
@@ -29,17 +49,21 @@ class ConnectedNewProject extends Component {
     }
 
     render(){
+        const {projectName} = this.props
         return (
             <div onClick={this.handleBackgroundClose} className="new-project-modal">
                 <div className="new-project-modal-input">
                     <h3>New project</h3>
                     <div className="new-project-form">
-                        form...
+                        <form>
+                            Project name:<br />
+                            <input type="text" id="projectName" value={projectName} onChange={this.handleInputChange}></input>
+                        </form>
                     </div>
 
                     <div className="buttons">
-                        <button className="button close" onClick={this.handleClose}>Cancel</button>
-                        <button className="button submit" onClick={this.handleSubmit}>Create</button>
+                        <Button styles="close" text="Cancel" onClick={this.handleClose} />
+                        <Button styles="submit" text="Submit" onClick={this.handleSubmit}/>
                     </div>
                 </div>
             </div>
