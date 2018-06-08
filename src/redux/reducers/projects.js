f869e490416d573f52c747598e2d6721e32e722e
Jody Duke <contact@jodyduke.co.uk>
2018-06-08 15:30:51 +0100

    changed redux project store from object to array

--- src/redux/reducers/projects.js
+++ src/redux/reducers/projects.js
@@ -1,10 +1,10 @@
 import { ADD_PROJECT } from '../constants/action-types';
 
 
-const projects = (state = {}, action) => {
+const projects = (state = [], action) => {
     switch(action.type) {
         case ADD_PROJECT:
-            return {...state, projects:[...state.projects, ...action.payload]};
+            return [...state, {...state.projects, ...action.payload}];
         default:
             return state;
     }
