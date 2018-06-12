3a232db2231763ad9ff98c51cc185b49ea5b5729
Jody Duke <contact@jodyduke.co.uk>
2018-06-12 12:24:51 +0100

    removed uneeded constructor

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import Sidebar from '../sidebar/sidebar'
 
 class Main extends Component {
-    constructor(){
-        super()
-    }
-
     render() {
         return (
             <div className="main">
